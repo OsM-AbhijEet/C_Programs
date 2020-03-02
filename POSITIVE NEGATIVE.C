2676c4ae715157e42302154ec414c39492d9bbe3
OsM-AbhijEet <61679065+OsM-AbhijEet@users.noreply.github.com>
2020-03-02 12:04:41 +0530

    Add files via upload

---
+++ POSITIVE NEGATIVE.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+ int a;
+ clrscr();
+ printf("enter the digit");
+ scanf("%d",&a);
+ if(a<0);
+ {
+ printf("you have entered the negative number");
+ }
+ if(a>0);
+ {
+ printf("you have entered the positive number");
+ }
+ return (0);
+ getch();
+ }
+
